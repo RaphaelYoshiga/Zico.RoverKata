35e670672eb8a80d7472129b177c614540facfd0
Raphael Yoshiga <Raphael.Yoshiga@asos.com>
2016-04-20 16:14:04 +0100

    Start with domain interfaces

---
+++ Zico.Training.KataRover.Domain.UnitTests/RoverShould.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Zico.Training.KataRover.Domain.UnitTests
+{
+    [TestFixture]
+    public class RoverShould
+    {
+        [Test]
+        public void DefaultPosition()
+        {
+            var rover = new Rover();
+
+            rover.Height.ShouldBe(0);
+            rover.Width.ShouldBe(0);
+            rover.CurrentDirection.ShouldBe(Direction.North);
+        }
+    }
+}
---
+++ Zico.Training.KataRover.Domain/Rover.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zico.Training.KataRover.Domain
+{
+    public class Rover
+    {
+        public Direction CurrentDirection { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+    }
+}
